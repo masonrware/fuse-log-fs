d8e4352587f7a3a873b1d2bc0d69a776e0934498
Ben Brandl <benbrandl82@gmail.com>
2023-11-30 17:13:52 -0600

    swapping functions

--- mkfs.wfs.c
+++ mkfs.wfs.c
@@ -1,33 +1,49 @@
-#define FUSE_USE_VERSION 30
-#include <fuse.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
+#include "wfs.h"
 
-static int my_getattr(const char *path, struct stat *stbuf) {
-    // Implementation of getattr function to retrieve file attributes
-    // Fill stbuf structure with the attributes of the file/directory indicated by path
-    // ...
+char* base;
 
-    return 0; // Return 0 on success
+void initialize_filesystem(const char *disk_path) {
+    int fd;
+
+    // Open file descriptor for file to init system with
+    fd = open(disk_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get file info (for file size)
+    struct stat file_stat;
+    if (fstat(fd, &file_stat) == -1) {
+        perror("fstat");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Memory map file
+    base = mmap(NULL, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+                fd, 0);
+
+    close(fd);
+    printf("Filesystem initialized successfully.\n");
 }
 
-static struct fuse_operations my_operations = {
-    .getattr = my_getattr,
-    // Add other functions (read, write, mkdir, etc.) here as needed
-};
-// static struct fuse_operations ops = {
-//     .getattr	= wfs_getattr,
-//     .mknod      = wfs_mknod,
-//     .mkdir      = wfs_mkdir,
-//     .read	    = wfs_read,
-//     .write      = wfs_write,
-//     .readdir	= wfs_readdir,
-//     .unlink    	= wfs_unlink,
-// };
-
 int main(int argc, char *argv[]) {
-    // Initialize FUSE with specified operations
-    // Filter argc and argv here and then pass it to fuse_main
-    return fuse_main(argc, argv, &my_operations, NULL);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <disk_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *disk_path = argv[1];
+    initialize_filesystem(disk_path);
+    
+    return 0;
 }

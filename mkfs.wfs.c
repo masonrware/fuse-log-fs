eece95606beb6c6ab4ee7b1130d2c4c3a11aa6f2
Ben Brandl <benbrandl82@gmail.com>
2023-12-01 19:14:32 -0600

    merging fuse functions

--- mkfs.wfs.c
+++ mkfs.wfs.c
@@ -8,8 +8,6 @@
 #include <errno.h>
 #include "wfs.h"
 
-char* base;
-
 void initialize_filesystem(const char *disk_path) {
     int fd;
 
@@ -29,31 +27,40 @@ void initialize_filesystem(const char *disk_path) {
     }
 
     // Memory map file
-    base = mmap(NULL, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+    char* base = mmap(NULL, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED,
                 fd, 0);
 
-    // initialize the superblock
-    // TODO pointer to superblock pointer -- have it point to base??
-    // TODO we will be using memcpy somewhere??
+    // Check for errors in mmap
+    if (base == MAP_FAILED) {
+        // TODO Handle error
+    }
 
-    struct wfs_sb superblock = {
-        .magic = WFS_MAGIC,
-        .head = 0
-    };
+    // initialize the superblock
+    struct wfs_sb* superblock = (struct wfs_sb*)base;
+
+    superblock->magic = WFS_MAGIC;
+    superblock->head = sizeof(struct wfs_sb);
 
     // Initialize the root directory log entry
     struct wfs_inode root_inode = {
         .inode_number = 0,
-        // TODO ... other fields ...
+        // TODO Other fields ...
     };
 
     struct wfs_log_entry root_log_entry = {
         .inode = root_inode,
     };
 
-    // TODO PUT THE SUPERBLOCK AND THE ROOT LOG ENTRY ON THE LOG (AFTER BASE) TO CALL MUNMAP WITH 
+    size_t root_log_entry_size = sizeof(struct wfs_log_entry) + /* size of 'data' field if any */;
 
-    // TODO call munmap to write to disk
+    // Place the root log entry at the head address
+    memcpy(base + superblock->head, &root_log_entry, root_log_entry_size);
+
+    // Update the head to be after the added root log entry
+    superblock->head += root_log_entry_size;
+    
+    // write to disk
+    munmap(base, file_stat.st_size);
 
     close(fd);
     printf("Filesystem initialized successfully.\n");

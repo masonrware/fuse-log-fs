5bf996a4b446d02865c5f15a407f49f6a9669c0a
Ben Brandl <benbrandl82@gmail.com>
2023-12-03 12:53:15 -0600

    iteration bound fix

--- mount.wfs.c
+++ mount.wfs.c
@@ -220,7 +220,19 @@ int canCreate(char *path){
     if (!isValidFilename(fname)) return 0;
 
     // Check if filename is unique in directory
-    
+    struct wfs_log_entry* parent = get_log_entry(isolate_path(path), 0);
+
+    struct wfs_dentry* data_addr = (struct wfs_dentry*)parent->data;
+
+    // iterate over all dentries
+    while(data_addr != (parent + parent->inode.size)) {
+        // check if current dentry matches desired filename
+        if (strcmp((*data_addr->name, fname) == 0) return 0;
+        data_addr += sizeof(struct wfs_dentry);
+    }
+
+    // filename is valid for given directory
+    return 1;
 }
 
 

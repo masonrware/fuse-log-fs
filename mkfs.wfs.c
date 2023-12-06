e852d9f77b026df479d41552dc745aaba37cd550
masonrware <86129546+masonrware@users.noreply.github.com>
2023-12-06 12:31:28 -0600

    some temp changes

--- mkfs.wfs.c
+++ mkfs.wfs.c
@@ -15,7 +15,7 @@ void initialize_filesystem(const char *disk_path) {
     int fd;
 
     // Open file descriptor for file to init system with
-    fd = open(disk_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    fd = open(disk_path, O_RDWR, 0666);
     if (fd == -1) {
         perror("Error opening file");
         exit(EXIT_FAILURE);
@@ -46,30 +46,27 @@ void initialize_filesystem(const char *disk_path) {
     superblock->head = sizeof(struct wfs_sb);
 
     // Initialize the root directory log entry
-    struct wfs_inode root_inode = {
-        .inode_number = 0,
-        .deleted = 0,
-        .mode = S_IFDIR,        // Set to S_IFDIR for a directory
-        .uid = getuid(),        // Set to the user id of the process
-        .gid = getgid(),        // Set to the group id of the process
-        .flags = 0,             // You can set flags based on your requirements
-        .size = 4096,           // Set to an appropriate size for a directory (e.g., 4 KB)
-        .atime = time(NULL),    // Set to the current time
-        .mtime = time(NULL),    // Set to the current time
-        .ctime = time(NULL),    // Set to the current time
-        .links = 0,             // Number of hard links
-    };
+    struct wfs_inode root_inode;
+    
+    root_inode.inode_number = 0;
+    root_inode.deleted = 0;
+    root_inode.mode = S_IFDIR;        // Set to S_IFDIR for a directory
+    root_inode.uid = getuid();        // Set to the user id of the process
+    root_inode.gid = getgid();        // Set to the group id of the process
+    root_inode.flags = 0;             // You can set flags based on your requirements
+    root_inode.size = 4096;           // Set to an appropriate size for a directory (e.g., 4 KB)
+    root_inode.atime = time(NULL);    // Set to the current time
+    root_inode.mtime = time(NULL);    // Set to the current time
+    root_inode.ctime = time(NULL);    // Set to the current time
+    root_inode.links = 0;             // Number of hard links
 
-    struct wfs_log_entry root_log_entry = {
-        .inode = root_inode,
-    };
+    struct wfs_log_entry* root_log_entry = (struct wfs_log_entry *)malloc(sizeof(struct wfs_log_entry));
+    root_log_entry->inode = root_inode;
 
     size_t root_log_entry_size = sizeof(struct wfs_log_entry);
 
     // Place the root log entry at the head address
-    uintptr_t conv = superblock->head;
-    void* head_ptr = (void*) conv;
-    memcpy(head_ptr, &root_log_entry, root_log_entry_size);
+    memcpy(superblock->head, root_log_entry, root_log_entry_size);
 
     // Update the head to be after the added root log entry
     superblock->head += root_log_entry_size;
@@ -89,6 +86,7 @@ int main(int argc, char *argv[]) {
     }
 
     const char *disk_path = argv[1];
+    printf("disk path: %s\n", disk_path);
     initialize_filesystem(disk_path);
     
     return 0;

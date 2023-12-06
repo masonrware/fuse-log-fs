f0fdf2fa91c4bdf048111854e89851fceeb2c883
Ben Brandl <benbrandl82@gmail.com>
2023-12-06 13:37:19 -0600

    removing statics

--- mount.wfs.c
+++ mount.wfs.c
@@ -16,21 +16,21 @@
 #include <time.h>
 #include "wfs.h"
 
-static int inode_count = 0;
-static int total_size;
+int inode_count = 0;
+int total_size;
 
-static char *disk_path;
-static char *mount_point;
+char *disk_path;
+char *mount_point;
 
-static char *head;
-static char *base;
-static struct wfs_sb *superblock;
+char *head;
+char *base;
+struct wfs_sb *superblock;
 
-static struct wfs_inode root_inode;
-static struct wfs_log_entry root_log_entry;
+struct wfs_inode root_inode;
+struct wfs_log_entry root_log_entry;
 
 // Remove the top-most (left most) extension of a path
-static char *snip_top_level(const char *path)
+char *snip_top_level(const char *path)
 {
     if (path == NULL || strlen(path) == 0)
     {
@@ -73,7 +73,7 @@ static char *snip_top_level(const char *path)
 }
 
 // Remove the bottom-most (right most) extension of a path
-static char *snip_bottom_level(const char *path)
+char *snip_bottom_level(const char *path)
 {
    if (path == NULL || mount_point == NULL || strlen(path) == 0 || strlen(mount_point) == 0)
     {
@@ -101,7 +101,7 @@ static char *snip_bottom_level(const char *path)
 
 // Same as snip_bottom_level, just returns the final part of the path instead of the path itself
 // Get bottom-level (right most) extention of a path
-static char *get_bottom_level(const char *path)
+char *get_bottom_level(const char *path)
 {
     if (path == NULL || strlen(path) == 0)
     {
@@ -134,7 +134,7 @@ static char *get_bottom_level(const char *path)
 }
 
 // Get the log entry of the bottom-level (right most) extension of a path recursively
-static struct wfs_log_entry *get_log_entry(const char *path, int inode_number)
+struct wfs_log_entry *get_log_entry(const char *path, int inode_number)
 {
     char *curr = base;
 
@@ -185,7 +185,7 @@ static struct wfs_log_entry *get_log_entry(const char *path, int inode_number)
 }
 
 // Remove any pre-mount portion (including the mount point) of a path
-static char *remove_pre_mount(const char *path)
+char *remove_pre_mount(const char *path)
 {
     if (path == NULL || mount_point == NULL || strlen(path) == 0 || strlen(mount_point) == 0)
     {
@@ -228,7 +228,7 @@ static char *remove_pre_mount(const char *path)
 
 // Check if filename contains valid characters
 // TODO check length as well?
-static int valid_name(const char *filename)
+int valid_name(const char *filename)
 {
     while (*filename != '\0')
     {
@@ -245,7 +245,7 @@ static int valid_name(const char *filename)
 }
 
 // Check if file/subdir can be created -- validate name and (local) uniqueness
-static int can_create(const char *path)
+int can_create(const char *path)
 {
     char *last_part = get_bottom_level(path);
 

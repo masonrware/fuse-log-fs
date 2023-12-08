dcf9d4e350ac76d227163c9503e02809c0b53789
masonrware <86129546+masonrware@users.noreply.github.com>
2023-12-08 17:11:07 -0600

    finished python script

--- test.py
+++ test.py
@@ -10,3 +10,24 @@ TEST_DIR = '/home/cs537-1/tests/P7'
 READONLY_PREBUILT_DISK = f'{TEST_DIR}/prebuilt_disk'
 TEST_PREBUILT_DISK = 'test_prebuilt_disk'
 NEW_DISK_PATH = f'd'
+
+
+def run_command(command, err_msg):
+    # Run the command and check if it returns 0
+    try:
+        ret = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=10)
+    except subprocess.TimeoutExpired:
+        print(f'Command {command} timed out')
+        return False
+    if ret.returncode != 0:
+        print(err_msg)
+        return False
+    return True
+
+def main():
+    # Mount the prebuilt image system
+    shutil.copyfile(READONLY_PREBUILT_DISK, TEST_PREBUILT_DISK)
+    run_command(f'./mount.wfs -s {TEST_PREBUILT_DISK} {MOUNT_POINT}', './mount.wfs returned non-zero exit code')
+
+if __name__ == "__main__":
+    main()

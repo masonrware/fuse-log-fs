696908a68e223276d6872b0f96d68dfa3ae8a085
masonrware <86129546+masonrware@users.noreply.github.com>
2023-12-08 17:13:53 -0600

    finished python script

--- test.py
+++ test.py
@@ -13,22 +13,22 @@ NEW_DISK_PATH = f'd'
 
 MOUNT_POINT = os.path.abspath('mnt')
 
-def run_command(command, err_msg):
-    # Run the command and check if it returns 0
-    try:
-        ret = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=10)
-    except subprocess.TimeoutExpired:
-        print(f'Command {command} timed out')
-        return False
-    if ret.returncode != 0:
-        print(err_msg)
-        return False
-    return True
+# def run_command(command, err_msg):
+#     # Run the command and check if it returns 0
+#     try:
+#         ret = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=10)
+#     except subprocess.TimeoutExpired:
+#         print(f'Command {command} timed out')
+#         return False
+#     if ret.returncode != 0:
+#         print(err_msg)
+#         return False
+#     return True
 
 def main():
     # Mount the prebuilt image system
     shutil.copyfile(READONLY_PREBUILT_DISK, TEST_PREBUILT_DISK)
-    run_command(f'./mount.wfs -s {TEST_PREBUILT_DISK} {MOUNT_POINT}', './mount.wfs returned non-zero exit code')
+    # run_command(f'./mount.wfs -s {TEST_PREBUILT_DISK} {MOUNT_POINT}', './mount.wfs returned non-zero exit code')
 
 if __name__ == "__main__":
     main()

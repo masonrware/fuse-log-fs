d27ef606ec75aff3f87417559f389bb5d1376eda
Ben Brandl <benbrandl82@gmail.com>
2023-12-08 17:41:16 -0600

    log ent print

--- mount.wfs.c
+++ mount.wfs.c
@@ -148,6 +148,18 @@ struct wfs_log_entry *get_log_entry(const char *path, int inode_number)
         if (curr_log_entry->inode.deleted != 1)
         {
             printf(">>get_log_entry: inode not deleted\n");
+            printf("curr_log_entry (%p) :: crl+size (%p) :: data_addr (%p) :: base (%p) :: head (%p)\n", (char *) curr_log_entry,(char *)(curr_log_entry) + curr_log_entry->inode.size, data_addr, base, head);
+            printf("curr_log_entry->inode_number: %d\n", curr_log_entry->inode.inode_number);
+            printf("curr_log_entry->deleted: %d\n", curr_log_entry->inode.deleted);
+            printf("curr_log_entry->mode: %d\n", curr_log_entry->inode.mode);
+            printf("curr_log_entry->uid: %d\n", curr_log_entry->inode.uid);
+            printf("curr_log_entry->gid: %d\n", curr_log_entry->inode.gid);
+            printf("curr_log_entry->flags: %d\n", curr_log_entry->inode.flags);
+            printf("curr_log_entry->size: %d\n", curr_log_entry->inode.size);
+            printf("curr_log_entry->atime: %d\n", curr_log_entry->inode.atime);
+            printf("curr_log_entry->mtime: %d\n", curr_log_entry->inode.mtime);
+            printf("curr_log_entry->ctime: %d\n", curr_log_entry->inode.ctime);
+            printf("curr_log_entry->links: %d\n", curr_log_entry->inode.links);
             // we found the log entry of the inode we need
             if (curr_log_entry->inode.inode_number == inode_number)
             {
